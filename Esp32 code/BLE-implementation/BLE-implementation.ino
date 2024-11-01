9f175251caeccc26dd6f9c00b12f6f8fd1f6a4bb
AiandioIfidon <150620311+AiandioIfidon@users.noreply.github.com>
2024-11-01 13:04:41 +0100

    Removed some sensitive info

--- Esp32 code/BLE-implementation/BLE-implementation.ino
+++ Esp32 code/BLE-implementation/BLE-implementation.ino
@@ -1,38 +1,24 @@
-#define BLYNK_TEMPLATE_ID "TMPL2qmaW4p6H"
-#define BLYNK_TEMPLATE_NAME "Led blink test"
-
-
 #include <Preferences.h>
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
 #include <WiFiClient.h>
-#include <BlynkSimpleEsp32.h>
-#include <pgmspace.h>
 #include <string>
+#include <pgmspace.h>
+#include "config.h"
 
+/*
+Imported variables through header file. Variables are ;
 
-// Constants
-constexpr char DEVICE_NAME[] = "Solar_Inverter";
-constexpr char PREF_NAMESPACE[] = "wifi_creds";
-constexpr size_t MAX_CREDENTIAL_LENGTH = 65;  // Increased for safety
+const char auth[] = ""; // Blynk authentication token
 
 // UUIDs
-constexpr char SERVICE_UUID[] = "853f29b2-f5ed-4b69-b4c6-9cd68a9fc2b0";
-constexpr char SSID_UUID[] = "b72b9432-25f9-4c7f-96cb-fcb8efde84fd";
-constexpr char PASSPHRASE_UUID[] = "7c8451c7-7909-47ef-b072-35d24729b8aa";
-
-
-const PROGMEM char auth[] = "Cka-31Akppx-UNnQUQQ5f5Bea6IVf1Kd";
-
-// Global variables with safer size
-char g_ssid[MAX_CREDENTIAL_LENGTH] = {0}; 
-char g_passphrase[MAX_CREDENTIAL_LENGTH] = {0};
+constexpr char SERVICE_UUID[] = "";
+constexpr char SSID_UUID[] = "";
+constexpr char PASSPHRASE_UUID[] = "";
+*/
 
 Preferences preferences;
-
-
-
 
  void storeWiFiCredentials() {
     if (!preferences.begin(PREF_NAMESPACE, false)) {
@@ -215,7 +201,7 @@ void loop() {
   Serial.print(ss);
   String pp = String(g_passphrase);
   Serial.println("Passphrase : \n");
-  Serial.print(pp);-
+  Serial.print(pp);
 
   digitalWrite(4, LOW);
   delay(2000);
